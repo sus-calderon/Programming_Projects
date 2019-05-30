8a528962628fcb3e5b6f8bee38c755038ecbce78
Susana C <scalderon@vt.edu>
2019-05-30 15:41:17 -0400

    This is my molecule class

---
+++ Project_1/molecule.h
@@ -0,0 +1,25 @@
+#include <string>
+
+using namespace std;
+
+class Molecule
+{
+    public:
+        //I'm declaring variables that can be used now
+        int natom;
+        int charge;
+        int *Zval;
+        double **geom;
+        string point_group;
+        //I'm declaring fucntions I want to use
+        void print_geom();  //Void indicates a function that doesn't return a value
+        void rotate(double phi);
+        void translate(double x, double y, double z);
+        double bond(int atom1, int atom2); //Use in step 2
+        double angle(int atom1, int atom2, int atom3);
+        double torsion(int atom1, int atom2, int atom3; int atom4);
+
+        Molecule();
+        ~Molecule();
+};
+
