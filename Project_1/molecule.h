5731d25fb838b8ab4bb8d174ea98469e49ff7a01
sus-calderon <scalderon@vt.edu>
2019-06-04 12:20:08 -0400

    The constructor took in two argument, so we need to modify its declaration in the .h file

--- Project_1/molecule.h
+++ Project_1/molecule.h
@@ -11,7 +11,8 @@ class Molecule
         int *zvals;
         double **geom;
         string point_group;
-        //I'm declaring fucntions I want to use
+        //I'm declaring functions I want to use
+
         void print_geom();  //Void indicates a function that doesn't return a value
         void rotate(double phi);
         void translate(double x, double y, double z);
@@ -19,7 +20,7 @@ class Molecule
         double angle(int atom1, int atom2, int atom3);
         double torsion(int atom1, int atom2, int atom3, int atom4);
 
-        Molecule();
+        Molecule(int n, int q);
         ~Molecule();
 };
 
