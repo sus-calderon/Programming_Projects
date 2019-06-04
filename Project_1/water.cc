0a0f9a28ce2dafc5e4e83ec969569fca5ca04a49
sus-calderon <scalderon@vt.edu>
2019-06-04 13:09:12 -0400

    So now that olecule.cc reads from an input file, my water.cc file does not need any coordinates explicitly written in its file.

--- Project_1/water.cc
+++ Project_1/water.cc
@@ -14,20 +14,7 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    Molecule h2o(3,0);  //So I can now input natoms and charge as natoms=3 and q=0
-
-    h2o.zvals[0] = 8;
-    h2o.geom[0][0] =  0.000000000000;
-    h2o.geom[0][1] =  0.000000000000;
-    h2o.geom[0][2] = -0.122368916506;
-    h2o.zvals[1] = 1;
-    h2o.geom[1][0] =  0.000000000000;
-    h2o.geom[1][1] =  1.414995841403;
-    h2o.geom[1][2] =  0.971041753535;
-    h2o.zvals[2] = 1;
-    h2o.geom[2][0] =  0.000000000000;
-    h2o.geom[2][1] = -1.414995841403;
-    h2o.geom[2][2] =  0.971041753535;
+    Molecule h2o("geom.dat",0);     //So this will read from the input file, what the zval is  
 
     h2o.print_geom();
     h2o.translate(5, 0, 0);
