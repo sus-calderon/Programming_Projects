c97c1ffebb545634ad158bee4b3ca34be87fab1d
sus-calderon <scalderon@vt.edu>
2019-06-04 12:01:09 -0400

    Fixed another typo

--- Project_1/molecule.cc
+++ Project_1/molecule.cc
@@ -7,7 +7,7 @@
 void Molecule::print_geom()
 {
     for(int i=0; i<natom; i++)
-        printf(%d %8.5f %8.5f %8.5f\n", zvals[i], geom[i][0], geom[i][1], geom[i][2]);
+        printf("%d %8.5f %8.5f %8.5f\n", zvals[i], geom[i][0], geom[i][1], geom[i][2]);
 }
 
 void Molecule::translate(double x, double y, double z)
