824e678196eaca468c7d09efe5202d0b379c674c
sus-calderon <scalderon@vt.edu>
2019-07-19 12:04:51 -0400

    Added back new variables for one electron integral matrices and core hamiltonian matrix.

--- Project_3/hartreefock.h
+++ Project_3/hartreefock.h
@@ -10,14 +10,16 @@ using namespace std;
 class HartreeFock     //So this class is specific to reading in Hessians (which don't contain Z values 
 {
     public:     //This mean that all the fxns and variables can be used by any code that makes use of an object called HartreeFock
-        int natom;
+        int norb;
         double **S; //The overlap integral is a 7x7 integral, and so s[i][j]= 1 1 1.00000 if i=j=1
-                    //My program is going to have to read in those values as i and j for my integral
+        //            //My program is going to have to read in those values as i and j for my integral
+        double **T;
+        double **V;
         double **core;  //My core Hamiltonian will be made up of the t and v (kinetic and potential)
 
-        string point_group;  
-
-        void print_matrix();      //To print out Hessian matrix of 2nd derivatives
+        void print_matrix(string mat_string, double** matrix);      //To print out Hessian matrix of 2nd derivatives
+        void read_oei(double** oei_mat, const char *filename);
+        void build_core(double** t_mat, double** v_mat);
 
         HartreeFock(const char *filename);
         ~HartreeFock();
