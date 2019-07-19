51c3e57474362e2d1a1f076492ed36bca3c5821c
sus-calderon <scalderon@vt.edu>
2019-07-19 15:36:43 -0400

    Declared enuc variable and read_enuc function so that main function has only one line calling for enuc data.

--- Project_3/hartreefock.h
+++ Project_3/hartreefock.h
@@ -10,6 +10,7 @@ using namespace std;
 class HartreeFock     
 {
     public:             //This mean that all the fxns and variables can be used by any code that makes use of an object called HartreeFock
+        double enuc;    //Nuclear repulsion energy
         int norb;       //Number of atomic orbitals in file (water has 7, 1s for each H, and 1s, 2s, and 3 2p for O)
         double **S;     //The overlap integral is a 7x7 integral, and so s[i][j]=1.00000 if i=j=1
                         //My program is going to have to read in those values as i and j for my integral
@@ -18,6 +19,7 @@ class HartreeFock
         double **core;  //My core Hamiltonian will be made up of the t and v (kinetic and potential)
 
         void print_matrix(string mat_string, double** matrix);      
+        void read_enuc(const char *filename);
         void read_oei(double** oei_mat, const char *filename);
         void build_core(double** t_mat, double** v_mat);
 
