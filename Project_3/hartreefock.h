c165728fde2131030b152a1ea4587a2065ad06af
sus-calderon <scalderon@vt.edu>
2019-07-29 17:08:02 -0400

    Changed my declared TEI function so it will use the Eigen Vector class and store it as a vector.

--- Project_3/hartreefock.h
+++ Project_3/hartreefock.h
@@ -18,20 +18,23 @@ class HartreeFock
     public:             //This mean that all the fxns and variables can be used by any code that makes use of an object called HartreeFock
         double enuc;    //Nuclear repulsion energy
         int norb;       //Number of atomic orbitals in file (water has 7, 1s for each H, and 1s, 2s, and 3 2p for O)
-        Matrix S;     //The overlap integral is a 7x7 integral, and so s[i][j]=1.00000 if i=j=1
+        Matrix S;       //The overlap integral is a 7x7 integral, and so s[i][j]=1.00000 if i=j=1
         Matrix T;
         Matrix V;
-        Matrix core;  //My core Hamiltonian will be made up of the t and v (kinetic and potential)
-        double *TEI;     //Two electron repulsion integral
+        Matrix core;    //My core Hamiltonian will be made up of the t and v (kinetic and potential)
+        Matrix SOM;     //The Symmetric Orthogonal Overlap Matrix
+        Vector TEI;    //Two electron repulsion integral
 
         void print_matrix(string mat_string, Matrix matrix);      
+
         void read_enuc(const char *filename);
         Matrix read_overlap(Matrix oei_mat, const char *filename);
         Matrix read_kinetic(Matrix oei_mat, const char *filename);
         Matrix read_potential(Matrix oei_mat, const char *filename);
+        Vector read_tei(Vector tei_ary, const char *filename);
+
         Matrix build_core(Matrix t_mat, Matrix v_mat);
-        void read_tei(double* tei_ary, const char *filename);
-        void build_orthog(Matrix s_mat);
+        Matrix build_orthog(Matrix s_mat);
         void build_fock_guess(double** s_ortho, Matrix core_mat);
 
         HartreeFock(const char *filename);
